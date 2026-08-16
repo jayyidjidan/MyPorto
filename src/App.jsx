45f5354e5abbfaa4e1ee385b1045e8a59d8f263e
Jayyid Jidan <mjayjidan14@gmail.com>
2026-08-16 16:48:51 +0700

    add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/ProjectDetail";
 import AdminHome from "./pages/Admin/Home";
+import { Analytics } from "@vercel/analytics/react";
 import AddProject from "./pages/Admin/AddProject";
 import EditProject from "./pages/Admin/EditProject";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/projects/:slug" element={<ProjectDetail />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects/:slug" element={<ProjectDetail />} />
 
-      <Route path="/admin" element={<AdminHome />} />
-      <Route path="/admin/add-project" element={<AddProject />} />
-      <Route path="/admin/edit-project/:slug" element={<EditProject />} />
-    </Routes>
+        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/admin/add-project" element={<AddProject />} />
+        <Route path="/admin/edit-project/:slug" element={<EditProject />} />
+      </Routes>
+
+      <Analytics />
+    </>
   );
 }
 
